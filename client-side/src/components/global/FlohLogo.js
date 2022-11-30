a29403bdcaf6aba047e36263d36ed0f8fd77ce91
chrisean nichols <chriseannichols550@gmail.com>
2022-11-30 17:57:33 -0500

    need to make logo appear

---
+++ client-side/src/components/global/FlohLogo.js
@@ -0,0 +1,7 @@
+const FlohLogo = () => {
+  return(
+    <img src='./../../assets/logos/Logo.svg' alt='floh suites'/>
+  );
+}
+
+export default FlohLogo;
---
+++ client-side/src/views/ComingSoon.js
@@ -0,0 +1,12 @@
+import FlohLogo from '../components/global/FlohLogo';
+import '../styles/coming_soon.css';
+
+const ComingSoon = () => {
+  return (
+    <section className='coming_soon'>
+      <FlohLogo />
+    </section>
+  );
+}
+
+export default ComingSoon;
